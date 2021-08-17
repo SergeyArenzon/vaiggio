cec19f6692016b2a37af1238472fdd068bc78f77
Sergey Arenzon <sergeyr1991@gmail.com>
2021-08-17 12:01:50 +0300

    creation location

--- components/ImagesUrlInput/ImagesUrlInput.js
+++ components/ImagesUrlInput/ImagesUrlInput.js
@@ -1,20 +1,36 @@
 export default function ImagesUrlInput({ setImageUrls, imageUrls }) {
 
+  const addUrlHandler = () => {
+    const newImageUrls = [...imageUrls];
+    newImageUrls.push("");
+    setImageUrls(newImageUrls);
+  };
+
+  const deleteHandler = (index) => {
+      const newImageUrls = [...imageUrls];
+      newImageUrls.splice(index, 1);
+      console.log(newImageUrls);
+    setImageUrls(newImageUrls);
+  };
+
+  const urlTypingHandler = (event, index) => {
+    const newImageUrls = [...imageUrls];
+    newImageUrls[index] = event.target.value;
+    setImageUrls(newImageUrls);
+  }
 
 
-    const addUrlHandler = () => {
-        const newImageUrls = [...imageUrls];
-        newImageUrls.push("");
-        setImageUrls(newImageUrls);
 
-    }
-  const currentUrls = imageUrls.map((url) => {
+  const currentUrls = imageUrls.map((url, index) => {
     return (
-      <li>
-        <input type="text" value={url}></input>
+      <li key={index}>
+        <input type="text" onChange={(event) => urlTypingHandler(event, index)} placeholder={index}></input>
+        <button onClick={() => deleteHandler(index)}>Delete</button>
       </li>
     );
   });
+
+
   return (
     <div>
       <ul>{currentUrls}</ul>
